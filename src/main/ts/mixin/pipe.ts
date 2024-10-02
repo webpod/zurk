b61be95b70c79536b2ce99ab096d0e96604c522f
Anton Golub <antongolub@antongolub.com>
2024-10-02 17:10:09 +0300

    feat: provide multipiping

--- src/main/ts/mixin/pipe.ts
+++ src/main/ts/mixin/pipe.ts
@@ -1,6 +1,7 @@
 import { Writable } from 'node:stream'
-import { assign } from '../util.js'
-import { VoidWritable } from '../spawn.js'
+import EventEmitter from 'node:events'
+import { assign, isStringLiteral } from '../util.js'
+import { VoidStream } from '../spawn.js'
 import type { TShell, TMixin, TShellCtx } from '../x.js'
 import { type TZurk, type TZurkPromise, isZurkAny } from '../zurk.js'
 
@@ -8,38 +9,47 @@ import { type TZurk, type TZurkPromise, isZurkAny } from '../zurk.js'
 export const pipeMixin: TMixin = <T extends TZurk | TZurkPromise >($: TShell, result: T, ctx: TShellCtx) =>
   isZurkAny(result)
     ? assign(result, {
-      pipe(...args: any[]) {
-        const target = args[0]
-        const { fulfilled, stdout, store } = ctx
+      pipe(...args: any) {
+        const [target, ...rest] = args
+        const { fulfilled, store, ee } = ctx
+        const from = new VoidStream()
+        const sync = !('then' in result)
+        const input = fulfilled ? fulfilled.stdout : from
+        const fill = () => {
+          for (const chunk of store.stdout) {
+            from.write(chunk)
+          }
+        }
+        let _result
+
         if (isZurkAny(target)) {
-          // stream.ctx.input = fulfilled.stdout
-          const input = new VoidWritable()
           target.ctx.input = input
-          for (const chunk of store.stdout) {
-            input.push(chunk)
-          }
-          if (fulfilled) {
-            input.push(null)
-          } else {
-            stdout.pipe(input)
-          }
-
-          return target
+          _result = target
+        } else if (target instanceof Writable) {
+          _result = from.pipe(target)
+        } else if (isStringLiteral(target, ...rest)) {
+          _result = $.apply({ input: input, sync}, args)
+        } else {
+          throw new Error('Unsupported pipe argument')
         }
 
-        if (target instanceof Writable) {
-          for (const chunk of store.stdout) {
-            target.write(chunk)
-          }
-          if (fulfilled) {
-            target.end()
-            return target
-          }
-
-          return stdout.pipe(target)
+        if (fulfilled) {
+          fill()
+          from.end()
+        } else {
+          const onStdout = (chunk: string | Buffer) => from.write(chunk)
+          ee
+            .once('stdout', () => {
+              fill()
+              ee.on('stdout', onStdout)
+            })
+            .once('end', () => {
+              ee.removeListener('stdout', onStdout)
+              from.end()
+            })
         }
 
-        return $.apply({input: fulfilled?.stdout || stdout, sync: !('then' in result)}, args as any)
+        return _result
       }
     })
     : result
