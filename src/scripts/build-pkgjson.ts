1d476707bc6fc080bf519b131e69ccc159851307
Anton Golub <antongolub@antongolub.com>
2025-09-21 17:38:36 +0300

    ci: add zizmor, separate release job, update actions, enable coverage store and docs publish

---
+++ src/scripts/build-pkgjson.ts
@@ -0,0 +1,38 @@
+import fs from 'node:fs'
+import path from 'node:path'
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const root = path.resolve(__dirname, '../..')
+const source = 'package.json'
+const dest = 'package-main.json'
+const _pkgJson = JSON.parse(fs.readFileSync(path.join(root, source), 'utf-8'))
+
+const whitelist = new Set([
+  'name',
+  'version',
+  'description',
+  'type',
+  'main',
+  'types',
+  'typesVersions',
+  'exports',
+  'bin',
+  'man',
+  'files',
+  'engines',
+  'optionalDependencies',
+  'publishConfig',
+  'keywords',
+  'repository',
+  'homepage',
+  'author',
+  'license',
+])
+
+const pkgJson = Object.fromEntries(
+  Object.entries(_pkgJson).filter(([k]) => whitelist.has(k))
+)
+
+fs.writeFileSync(path.resolve(root, dest), JSON.stringify(pkgJson, null, 2))
+
+console.log(`${dest} prepared for npm`)
