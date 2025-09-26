ae8465704ccaa1901a6bb53d61ed7ef63e2c4271
Anton Golub <antongolub@antongolub.com>
2025-09-26 14:19:26 +0300

    fix: fix release flow & repack

--- src/scripts/build-pkgjson.ts
+++ src/scripts/build-pkgjson.ts
@@ -4,7 +4,7 @@ import path from 'node:path'
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
 const root = path.resolve(__dirname, '../..')
 const source = 'package.json'
-const dest = 'package-main.json'
+const dest = process.env.PKGJSON_PATH || 'package-main.json'
 const _pkgJson = JSON.parse(fs.readFileSync(path.join(root, source), 'utf-8'))
 
 const whitelist = new Set([
@@ -12,6 +12,7 @@ const whitelist = new Set([
   'version',
   'description',
   'type',
+  'module',
   'main',
   'types',
   'typesVersions',
@@ -29,10 +30,11 @@ const whitelist = new Set([
   'license',
 ])
 
-const pkgJson = Object.fromEntries(
-  Object.entries(_pkgJson).filter(([k]) => whitelist.has(k))
-)
+const pkgJson =
+  Object.fromEntries(
+    Object.entries(_pkgJson)
+      .filter(([k]) => whitelist.has(k)))
 
-fs.writeFileSync(path.resolve(root, dest), JSON.stringify(pkgJson, null, 2))
+fs.writeFileSync(path.resolve(root, dest), JSON.stringify(pkgJson, null, 2) + '\n')
 
 console.log(`${dest} prepared for npm`)
